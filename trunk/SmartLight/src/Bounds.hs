648ba5671f1e3a10789fdcf1cdd841fe9f1b12b7
sawady.faso <sawady.faso@0532ff01-83b2-013b-6084-84fc13803cbb>
2013-09-16 05:18:28 +0000

    Bounds added to Entity

--- trunk/SmartLight/src/Bounds.hs
+++ trunk/SmartLight/src/Bounds.hs
@@ -1,43 +1,29 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Bounds where
 
-import Entity
-import Control.Lens.TH
+import Common
 import Control.Lens
 
-data Bounds = RBound RectBound | CBound CircleBound
-
-data RectBound = RectBound {
-      _bPos :: Pos
-    , _bW   :: Int
+data Bounds = Bounds {
+      _bW   :: Int
     , _bH   :: Int
 }
 
-data CircleBound = CircleBound {
-      _cPos :: Pos
-    , _cR   :: Int
-}
+makeLenses ''Bounds
 
-makeLenses ''RectBound
-makeLenses ''CircleBound
-
-collideWithBounds :: Bounds -> Bounds -> Bool
-collideWithBounds (RBound b1) (RBound b2) = collidesRectAgainstRect b1 b2
-collideWithBounds _           _           = False
-
-collidesRectAgainstRect :: RectBound -> RectBound -> Bool
-collidesRectAgainstRect b1 b2 = 
+collideWithBounds :: Pos -> Bounds -> Pos -> Bounds -> Bool
+collideWithBounds p1 b1 p2 b2 = 
     (x1 <= x2 && x2 < right1 || x2 <= x1 && x1 < right2) 
     && (y1 <= y2 && y2 < bottom1 || y2 <= y1 && y1 < bottom2)                    
         where right1 = x1 + d
               right2 = x2 + f
               bottom1 = y1 + e
               bottom2 = y2 + g
-              x1 = view (bPos.pX) b1
-              y1 = view (bPos.pY) b1
+              x1 = view pX p1
+              y1 = view pY p1
               d  = view bW b1 
               e  = view bH b1
-              x2 = view (bPos.pX) b2
-              y2 = view (bPos.pY) b2
+              x2 = view pX p2
+              y2 = view pY p2
               f  = view bW b2
               g  = view bH b2
--- trunk/SmartLight/src/Entity.hs
+++ trunk/SmartLight/src/Entity.hs
@@ -1,30 +1,21 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Entity where
 
+import Bounds
+import Common
 import Control.Lens.TH
 import Control.Lens
-
-data Pos = Pos {
-      _pX :: Int
-    , _pY :: Int
-}
-
-data Vel = Vel {
-      _vX :: Int
-    , _vY :: Int
-}
 
 data Entity a = Entity {
       _entityName  :: String
     , _pos         :: Pos
     , _vel         :: Vel
+    , _bounds      :: Bounds
     , _entityData  :: a
 }
 
 type PureEntity = Entity ()
 
-makeLenses ''Pos
-makeLenses ''Vel
 makeLenses ''Entity
 
 newPureEntity :: String -> PureEntity
@@ -35,6 +26,7 @@ newEntity d n = Entity {
       _entityName = n
     , _pos        = Pos 0 0
     , _vel        = Vel 0 0
+    , _bounds     = Bounds 0 0
     , _entityData = d
 }
 
@@ -56,22 +48,22 @@ velSubX e = over (pos.pX) (\x -> x - view (vel.vX) e) e
 velSub :: Entity a -> Entity a
 velSub e = velSubX . velSubY $ e
 
-bounceX :: Entity a -> Entity a
-bounceX = (\b' -> over (pos . pX) (+ view (vel . vX) b' * 2) b') . over (vel  . vX) (* (-1))
+bounceX :: Entity a -> Entity b -> Entity a
+bounceX e1 e2 = (\b' -> over (pos . pX) (+ view (vel . vX) b' * 2) b') . over (vel  . vX) (* (-1)) $ e1
 
-bounceY :: Entity a -> Entity a
-bounceY = (\b' -> over (pos . pY) (+ view (vel . vY) b' * 2) b') . over (vel  . vY) (* (-1))
+bounceY :: Entity a -> Entity b -> Entity a
+bounceY e1 e2 = (\b' -> over (pos . pY) (+ view (vel . vY) b' * 2) b') . over (vel  . vY) (* (-1)) $ e1
 
 bounceOnEdgeX :: Int -> Entity a ->Entity a
 bounceOnEdgeX screenSizeX b = 
     if onEdgeX screenSizeX b 
-       then bounceX b
+       then bounceX b b
        else b
 
 bounceOnEdgeY :: Int -> Entity a -> Entity a
 bounceOnEdgeY screenSizeY b = 
     if onEdgeY screenSizeY b  
-       then bounceY b
+       then bounceY b b
        else b
 
 onEdgeX :: Int -> Entity a ->Bool
@@ -81,3 +73,6 @@ onEdgeX screenSizeX e = x < (- screenSizeX `div` 2) || x > (screenSizeX `div` 2)
 onEdgeY :: Int -> Entity a -> Bool
 onEdgeY screenSizeY e = y < (- screenSizeY `div` 2) || y > (screenSizeY `div` 2)
     where y = view (pos . pY) e
+    
+collideWith :: Entity a -> Entity b -> Bool
+collideWith e1 e2 = collideWithBounds (view pos e1) (view bounds e1) (view pos e1) (view bounds e2)
